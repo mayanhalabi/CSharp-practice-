f6c50e1e94051f0113d8534fec325b73f8f3f6e2
Mayan Halabi <mayanhalabi@stu.khas.edu.tr>
2026-09-10 20:53:58 +0300

    Add Program.cs for lecture 5

---
+++ lec5/Program.cs
@@ -0,0 +1,17 @@
+﻿int no1, no2, resultAdd=0, resultMulti=1;
+
+Console.Write("please enter the first number:");
+no1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("please enter the secound number:");
+no2 = Convert.ToInt32(Console.ReadLine());
+
+resultAdd = no1 + no2;
+resultMulti = no1 * no2;
+
+//Console.WriteLine("The result of adding operation is: ",resultAdd );
+//Console.WriteLine("The result of multi operation is: ",resultMulti );
+Console.WriteLine(resultAdd);
+Console.WriteLine(resultMulti);
+
