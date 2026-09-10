5583cdc405caa98494fea3a88f54740b366334ae
Mayan Halabi <mayanhalabi@stu.khas.edu.tr>
2026-09-10 20:26:36 +0300

    Add lecture 5

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿using System.Runtime.CompilerServices;
+﻿int no1, no2, resultAdd=0, resultMulti=1;
 
-int no1 , no2, result =0;
-no1=1;
-no2=3;
-result=no1+no2;
-Console.WriteLine(result);
+Console.Write("please enter the first number:");
+no1 = Convert.ToInt32(Console.ReadLine());
 
 
+Console.Write("please enter the secound number:");
+no2 = Convert.ToInt32(Console.ReadLine());
 
+resultAdd = no1 + no2;
+resultMulti = no1 * no2;
+
+//Console.WriteLine("The result of adding operation is: ",resultAdd );
+//Console.WriteLine("The result of multi operation is: ",resultMulti );
+Console.WriteLine(resultAdd);
+Console.WriteLine(resultMulti);
 
